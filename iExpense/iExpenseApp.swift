d39d539a24767ec69be256baac9eddf83f3fb1c0
Bora Kulak <91684506+Bcrusd@users.noreply.github.com>
2026-05-26 19:31:07 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Bora Kulak on 26.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
